87e3cbc4b35469da257fe238459b9f5331d34c61
Byte-nana <nanadevelop44@gmail.com>
2026-01-01 22:42:13 +0000

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from 'react-router';
 import './App.css';
+import Header from './components/Header.jsx';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import './index.css';
+import App from './App.jsx';
+import NotFound from './pages/NotFound.jsx';
+import Videos from './pages/Videos.jsx';
+import VideoDetail from './pages/VideoDetail.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, Component: Videos },
+      { path: '/videos', Component: Videos },
+      { path: '/videos/:keyword', Component: Videos },
+      { path: '/videos/watch/:videoId', Component: VideoDetail },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
